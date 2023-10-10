231fbbfa2ca064cc37aeb97dd54fbbfe7306f54f
Daniel <daniel@Daniels-MacBook-Air.local>
2023-10-10 22:16:36 +0200

    school stuff

---
+++ School/Get Max/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine(GetMax(30, 30, 40));
+
+static int GetMax(int num1, int num2, int num3)
+{
+    int result;
+    if (num1 >= num2 && num1 >= num3) {
+        result = num1;
+    }else if (num2 >= num1 && num2 >= num3 )
+    {
+        result = num2;
+    }else
+    {
+        result = num3;
+    }
+    
+
+    return result;
+}
